bd971f5611c6d35c5b264a29021ce04b7d2f2999
Ahmad Zaki <ahmadzak1009@bsi.ac.id>
2019-11-19 16:49:14 +0700

    menambahkan header

--- src/content/App.js
+++ src/content/App.js
@@ -3,7 +3,44 @@ import "../App.css";
 
 class App extends Component {
   render() {
-    return <div id="wrapper"></div>;
+    return (
+      <div id="wrapper">
+        <div id="header">
+          <div className="container flex-center-between">
+            <div id="logo">IndiHouse</div>
+            <div id="menu">
+              <ul id="menu-links" className="flex-center-between">
+                <li>
+                  <a href="#" className="menu-link">
+                    Promo
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="menu-link">
+                    Paket & Add On
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="menu-link">
+                    Pusat Bantuan
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="menu-link">
+                    Info Terkini
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="menu-link">
+                    MyIndiHouse
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
