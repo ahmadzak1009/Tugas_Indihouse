86835d20d9a2d18f804b018c2da6cf8e496908bd
Ahmad Zaki <ahmadzak1009@bsi.ac.id>
2019-11-19 20:38:12 +0700

    Updates

---
+++ precache-manifest.13293bdf6c0a71c8eb9a5930199b3f86.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aba8875a630ca18478ae33d0c262f1b",
+    "url": "/Tugas_Indihouse/index.html"
+  },
+  {
+    "revision": "e547e1c8b31a3a5cd8dc",
+    "url": "/Tugas_Indihouse/static/css/main.74579851.chunk.css"
+  },
+  {
+    "revision": "b72d241cde748e183e13",
+    "url": "/Tugas_Indihouse/static/js/2.94a5eeca.chunk.js"
+  },
+  {
+    "revision": "e547e1c8b31a3a5cd8dc",
+    "url": "/Tugas_Indihouse/static/js/main.024482f4.chunk.js"
+  },
+  {
+    "revision": "381ddb6cfcdf117ab132",
+    "url": "/Tugas_Indihouse/static/js/runtime-main.1b270275.js"
+  },
+  {
+    "revision": "e88c382c2678ea5c7a688994479aec0a",
+    "url": "/Tugas_Indihouse/static/media/2nd-slider-1.e88c382c.jpg"
+  },
+  {
+    "revision": "d7bac9675169aeeb4cdd4385f9306bb9",
+    "url": "/Tugas_Indihouse/static/media/2nd-slider-2.d7bac967.jpg"
+  },
+  {
+    "revision": "594a350bc3da9af522abdbf1fda059ca",
+    "url": "/Tugas_Indihouse/static/media/2nd-slider-3.594a350b.jpg"
+  },
+  {
+    "revision": "d352b85689563ce0a2112de6501cd6d2",
+    "url": "/Tugas_Indihouse/static/media/2nd-slider-4.d352b856.jpg"
+  },
+  {
+    "revision": "b41feeb350f628f19e46654e21b28436",
+    "url": "/Tugas_Indihouse/static/media/slider1.b41feeb3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Tugas_Indihouse/precache-manifest.8c5bee31b3c919bae31cf67dcf7a5b4f.js"
+  "/Tugas_Indihouse/precache-manifest.13293bdf6c0a71c8eb9a5930199b3f86.js"
 );
 
 self.addEventListener('message', (event) => {
